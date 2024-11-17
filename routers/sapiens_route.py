5cb2a04d0c298df8bf4783cf0f0b184e83e0741f
Tom Lai <tomlaihosum@gmail.com>
2024-11-18 00:24:45 +0800

    fix optional form data field

--- routers/sapiens_route.py
+++ routers/sapiens_route.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Optional, List
+from typing import Annotated, Optional, List, Dict, Any
 from fastapi import APIRouter, File, HTTPException, UploadFile, WebSocket, Form
 from tqdm import tqdm
 from models.classes_and_palettes import GOLIATH_CLASSES, GOLIATH_PALETTE
@@ -33,7 +33,7 @@ router = APIRouter()
 
 # Image segmentation for a single image
 @router.post('/sapiens-seg-img')
-async def sapiens_func(file: UploadFile, ctp: List[str] | None = None):
+async def sapiens_func(file: UploadFile, ctp: Optional[List[str]] = None) -> Dict[str, Any]:
     global model
     
     # skin_classes = [2,4,5,6,7,10,11,13,14,15,16,19,20,21]
