2b7ebee01304a9767064e5537193d5127ceebd99
Tom Lai <tomlaihosum@gmail.com>
2024-11-17 23:07:30 +0800

    bug fi for optional parameter

--- routers/sapiens_route.py
+++ routers/sapiens_route.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Union
+from typing import Annotated, Optional
 from fastapi import APIRouter, File, HTTPException, UploadFile, WebSocket, Form
 from tqdm import tqdm
 from models.classes_and_palettes import GOLIATH_CLASSES, GOLIATH_PALETTE
@@ -33,7 +33,7 @@ router = APIRouter()
 
 # Image segmentation for a single image
 @router.post('/sapiens-seg-img')
-async def sapiens_func(file: UploadFile, ctp:Annotated[list, Form()] = [2,4,5,6,7,10,11,13,14,15,16,19,20,21]):
+async def sapiens_func(file: UploadFile, ctp:Optional[list] = [2,4,5,6,7,10,11,13,14,15,16,19,20,21]):
     global model
     
     # skin_classes = [2,4,5,6,7,10,11,13,14,15,16,19,20,21]
